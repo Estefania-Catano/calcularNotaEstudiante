b3f62c8799e8a6276a13e96d8a17b5f3d677910d
martinezzzzzzzzzz <tu_correo_de_github@example.com>
2025-04-09 21:15:12 -0500

    Se define menú de opciones

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,42 @@
 import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-scanner sc = new Scanner(System.in);
-        string nombreEstudiante = "";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del estudiante: ");
+        String nombreEstudiante = sc.nextLine();
+        System.out.println("Ingrese el documento de identidad del estudiante: ");
+        String documentoEstudiante = sc.nextLine();
+        System.out.println("Seleccione el programa: \n1 - Desarrollo de Software\n2 - Diseño Grafico\n3 - Gastronomía");
+        String programa = sc.nextLine();
+        switch (programa) {
+            case "1":
+                System.out.println("Programa de desarrollo de software");
+                System.out.println("Seleccione la materia:\n1 - Lógica de programación\n2 -Metodologías ágiles\n3 - Introducción a la programación");
+                String materia = sc.nextLine();
+                switch (materia) {
+                    case "1":
+                        System.out.println("Nota de Lógica de programación");
+                        break;
+                    case "2":
+                        System.out.println("Nota de Metodologías ágiles");
+                        break;
+                    case "3":
+                        System.out.println("Nota de Introducción a la programación");
+                        break;
+                }
+                break;
+            case "2":
+                System.out.println("Programa diseño Grafico");
+                System.out.println("Seleccione la materia:\n1 - Tipografía\n2 - Iconografía\n3 - Semántica");
+                String materia = sc.nextLine();
+                switch (materia) {
+                    case "1":
+                        System.out.println("Nota de tipografía");
+                }
+                break;
+            case "3":
+                System.out.println("Programa Gastronomía");
+                break;
+        }
     }
 }
